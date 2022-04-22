fe3ceb620008047e48a7a4ab61c0d23918615daa
Shail079 <91492700+Shail079@users.noreply.github.com>
2022-04-22 11:27:42 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import numpy as np
 import pickle
-import pandas as pd
-from flask import Flask, request
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
@@ -19,11 +17,11 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
+    wheat_labels = {1: 'Kama', 2: 'Rosa', 3: 'Canadian'}
     int_features = [x for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = classifier.predict(final_features)
-
-    return render_template('index.html', prediction_text='The Wheat belong to class {}'.format(prediction))
+    return render_template('index.html', prediction_text='The wheat seed belongs to class {}'.format(wheat_labels[prediction[0]]))
 
 
 if __name__ == '__main__':
